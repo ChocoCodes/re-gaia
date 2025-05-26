9323c45db2e0783611f5ed3768b05e091b5b9de4
ChocoCodes <chocojhong999@gmail.com>
2025-05-27 00:39:48 +0800

    Chore: Spike Traps

---
+++ re-gaia/Assets/Scripts/Utils/SpikeTrap.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+    [Header("Spike Trap Config")]
+    [SerializeField] private int damage;
+    [SerializeField] private float activeTime;
+    [SerializeField] private float cd;
+
+    private Animator animator;
+    private bool isActive;
+    private bool isTriggered;
+    private float lastDamageTime = Mathf.NegativeInfinity;
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            Debug.Log("Spike Trap Triggered - Enter");
+
+            // Start activation ONLY if not already triggered
+            if (!isTriggered) {
+                StartCoroutine(ActivateSpike());
+            }
+
+            // Damage player if active and cooldown passed
+            if (isActive && Time.time - lastDamageTime >= cd) {
+                Debug.Log("Player hit by Spike Trap (Enter)");
+                other.GetComponent<PlayerHealth>().TakeDamage(damage);
+                lastDamageTime = Time.time;
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            // Start activation and animation if not triggered
+            if (!isTriggered) {
+                Debug.Log("Spike Trap Triggered - Stay, starting activation");
+                StartCoroutine(ActivateSpike());
+            }
+
+            // Damage player if active and cooldown passed
+            if (isActive && Time.time - lastDamageTime >= cd) {
+                Debug.Log("Player hit by Spike Trap (Stay)");
+                other.GetComponent<PlayerHealth>().TakeDamage(damage);
+                lastDamageTime = Time.time;
+            }
+        }
+    }
+
+    IEnumerator ActivateSpike() {
+        isTriggered = true;
+        isActive = true;
+        animator.SetBool("activated", true);
+
+        yield return new WaitForSeconds(activeTime);
+
+        isActive = false;
+        isTriggered = false;
+        animator.SetBool("activated", false);
+    }
+}
